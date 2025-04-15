981cd14f5d2cadf0a9c26ea26fdcd312e436117f
Maria Baburina <maria.baburina@jetbrains.com>
2025-04-15 16:31:13 +0200

    updated path to clang

--- main.cxx
+++ main.cxx
@@ -4,6 +4,7 @@ void hello_world(std::string const& name);
 
 int main(int argc, char* argv[])
 {
-    hello_world(argv[0] ? argv[0] : "Voldemort?");
+    hello_world(argv[1] ? argv[1] : "What?");
+    hello_world(argv[1] ? argv[1] : "Who?");
     return 0;
 }
